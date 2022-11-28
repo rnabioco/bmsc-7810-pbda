0730410f0f68b7dc629d521b050cf2324aad5c63
Kristen Wells <kristen.wells@cuanschutz.edu>
2022-11-28 16:51:15 -0700

    adding script to download files

---
+++ _posts/2022-11-10-class-7-matricies/download_files.R
@@ -0,0 +1,18 @@
+library(here)
+data_dir <- here("class_7-9_data")
+dir.create(data_dir, showWarnings = FALSE)
+
+files_to_dl <- list(
+  "boy_name_counts.csv" = "https://raw.githubusercontent.com/kwells4/pdba_files/main/files/boy_name_counts.csv",
+  "girl_name_counts.csv" = "https://raw.githubusercontent.com/kwells4/pdba_files/main/files/girl_name_counts.csv",
+  "state_info.csv" = "https://raw.githubusercontent.com/kwells4/pdba_files/main/files/state_information.csv"
+)
+
+missing_files <- files_to_dl[!file.exists(file.path(data_dir, names(files_to_dl)))]
+
+tout_setting <- getOption("timeout")
+options(timeout = 6000)
+
+for(id in names(missing_files)){
+  download.file(missing_files[[id]], file.path(data_dir, id))
+}
