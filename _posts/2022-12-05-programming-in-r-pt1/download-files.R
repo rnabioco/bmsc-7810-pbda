ce4b8b157f6760460973bb09389e0827192b2dff
Kent Riemondy <kent.riemondy@gmail.com>
2022-12-12 09:54:17 -0700

    class10 resources

---
+++ _posts/2022-12-05-programming-in-r-pt1/download-files.R
@@ -0,0 +1,18 @@
+data_dir <- "data"
+dir.create(data_dir, showWarnings = FALSE)
+
+files_to_dl <- list(
+  "brauer_gene_exp.csv.gz" = "https://github.com/rnabioco/bmsc-7810-pbda/raw/main/data/class10/brauer_gene_exp.csv.gz",
+  "yeast_prot_prop.csv.gz" = "https://github.com/rnabioco/bmsc-7810-pbda/raw/main/data/class10/yeast_prot_prop.csv.gz"
+)
+
+missing_files <- files_to_dl[!file.exists(file.path(data_dir, names(files_to_dl)))]
+
+tout_setting <- getOption("timeout")
+options(timeout = 6000)
+
+for(id in names(missing_files)){
+  download.file(missing_files[[id]], file.path(data_dir, id))
+}
+
+options(timeout = tout_setting)
