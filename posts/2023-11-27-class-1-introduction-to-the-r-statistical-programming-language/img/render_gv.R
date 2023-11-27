e1b5fd3e75580ab0e9c89f5ff3a94e17890264a6
kriemo <kent.riemondy@gmail.com>
2023-11-27 20:24:50 +0000

    Deploying to gh-pages from @ rnabioco/bmsc-7810-pbda@d23a56b257c23a79194cd1ab4711d8ed0e85129f 🚀

---
+++ posts/2023-11-27-class-1-introduction-to-the-r-statistical-programming-language/img/render_gv.R
@@ -0,0 +1,17 @@
+library(DiagrammeR)
+library(DiagrammeRsvg)
+library(rsvg)
+library(here)
+library(tiff)
+wd <- "_posts/2023-11-27-class-1-introduction-to-the-r-statistical-programming-language/img"
+gv_fn <- here(file.path(wd, "reco.gv"))
+
+inch_to_px <- function(x, dpi = 300) { x * dpi}
+h_inch <- 5
+asp_ratio <- 2.0
+h <- inch_to_px(h_inch)
+w <- inch_to_px(h_inch * asp_ratio)
+grViz(gv_fn, width = w, height = h) %>%
+  export_svg %>%
+  charToRaw %>%
+  rsvg_pdf(file.path(wd, "r-ecosystem.pdf"))
