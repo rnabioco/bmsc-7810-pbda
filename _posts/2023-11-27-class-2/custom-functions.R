0349d99ad81549eac5033a3f5056085d7b917f50
Kent Riemondy <kent.riemondy@gmail.com>
2023-11-29 13:02:05 -0700

    pub c2

--- _posts/2023-11-27-class-2/custom-functions.R
+++ _posts/2023-11-27-class-2/custom-functions.R
@@ -1,4 +1,14 @@
+
+important_list <- list(
+  best_number    = 42,
+  favorite_color = "purple",
+  best_day       = Sys.Date(),
+  todo           = "All things, all the time"
+)
+
 greeting <- function(name){
   msg <- paste("howdy", name, "!")
   print(msg)
 }
+
+
