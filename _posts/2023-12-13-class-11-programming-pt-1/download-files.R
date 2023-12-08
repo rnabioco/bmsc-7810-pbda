6c828c60610c6b399a71c233f07a587d91be8f7b
Ryan Sheridan <ryan.sheridan@cuanschutz.edu>
2023-12-08 14:57:16 -0700

    update links class 11

--- _posts/2023-12-13-class-11-programming-pt-1/download-files.R
+++ _posts/2023-12-13-class-11-programming-pt-1/download-files.R
@@ -2,8 +2,8 @@ data_dir <- "data"
 dir.create(data_dir, showWarnings = FALSE)
 
 files_to_dl <- list(
-  "brauer_gene_exp.csv.gz" = "https://github.com/rnabioco/bmsc-7810-pbda/raw/main/data/class10/brauer_gene_exp.csv.gz",
-  "yeast_prot_prop.csv.gz" = "https://github.com/rnabioco/bmsc-7810-pbda/raw/main/data/class10/yeast_prot_prop.csv.gz"
+  "brauer_gene_exp.csv.gz" = "https://github.com/rnabioco/bmsc-7810-pbda/raw/main/data/class11/brauer_gene_exp.csv.gz",
+  "yeast_prot_prop.csv.gz" = "https://github.com/rnabioco/bmsc-7810-pbda/raw/main/data/class11/yeast_prot_prop.csv.gz"
 )
 
 missing_files <- files_to_dl[!file.exists(file.path(data_dir, names(files_to_dl)))]
